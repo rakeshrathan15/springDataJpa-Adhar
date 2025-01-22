41939835b60e7b10b14fe32532800736e3424f7a
rakesh mekala <rakeshyadavmekala15@gmail.com>
2025-01-22 10:57:54 +0530

    first commit

---
+++ src/main/java/com/neoteric/springbootjpa_adhar/SpringbootjpaAdharApplication.java
@@ -0,0 +1,13 @@
+package com.neoteric.springbootjpa_adhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjpaAdharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjpaAdharApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoteric/springbootjpa_adhar/SpringbootjpaAdharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoteric.springbootjpa_adhar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjpaAdharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
